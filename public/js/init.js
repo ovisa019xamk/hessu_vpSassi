47fbfcd97c0584d640edd061834995c94c1cf33f
VP Sassi <ovisa019@edu.xamk.fi>
2018-04-03 16:08:05 +0300

    ateriavalikkosivun pohja

--- public/js/init.js
+++ public/js/init.js
@@ -5,3 +5,22 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+// OMA JQUREY
+$(document).ready(function(){
+	
+	// kategoriapalkin toiminto
+	$("#ateriaPalkki").collapsible();
+		
+	// etusivunapin painallus
+    $("#etusivuNappi").click(function(){
+		$("#ateriaValikko").css("display", "none"); // piilotetaan ateriavalikko
+		$("#etuSivu").css("display", "block"); // näytetään etusivu
+	});
+	// ateriavalikkonapin painallus
+	$("#ateriaNappi").click(function(){
+		$("#ateriaValikko").css("display", "block"); // näytetään ateriavalikko
+		$("#etuSivu").css("display", "none"); // piilotetaan etusivu
+	});
+});
