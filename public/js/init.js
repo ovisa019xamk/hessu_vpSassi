241be6d111a1c34320978e788bdcc3a4b03ebfc2
VP Sassi <ovisa019@edu.xamk.fi>
2018-04-04 11:49:58 +0300

    muokkauksia sivujen pohjaan

--- public/js/init.js
+++ public/js/init.js
@@ -7,12 +7,20 @@
 })(jQuery); // end of jQuery name space
 
 
-// OMA JQUREY
+// OMA JQUERY
 $(document).ready(function(){
 	
 	// kategoriapalkin toiminto
 	$("#ateriaPalkki").collapsible();
-		
+
+//************************************************************************************
+//TESTAUS
+
+	// näytä kaikki napin painallus
+	$("#kaikkiNappi").click(function(){ // näytetään kaikki sivut
+		$("#ateriaValikko").css("display", "block");
+		$("#etuSivu").css("display", "block");
+	});
 	// etusivunapin painallus
     $("#etusivuNappi").click(function(){
 		$("#ateriaValikko").css("display", "none"); // piilotetaan ateriavalikko
