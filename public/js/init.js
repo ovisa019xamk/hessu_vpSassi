aa6e0fe51eb19aaa2b2519a30ed7d897941d4a0d
VP Sassi <ovisa019@edu.xamk.fi>
2018-04-11 11:53:49 +0300

    ateriat listattu oikeisiin kategorioihin

--- public/js/init.js
+++ public/js/init.js
@@ -21,6 +21,91 @@ $(document).ready(function(){
 	
 	// omaa koodia
 	
+	$.ajax({
+		method: "GET", //haetaan
+		url: "http://localhost:8000/api/", // data json
+		success: function(result){
+			
+			tulosta(result);
+		},
+		error: function(xhr){
+			console.log("Virhe!");
+		}
+	});
+	
+	function tulosta(result){
+		
+		var ateriaMenu = result.ruokalista;
+		
+		// käydään läpi kaikki ruokalistan elementit
+		ateriaMenu.forEach(function(result){
+			// liharuokakategorian ateriat
+			if(result.ruokalaji == "liharuoka"){
+				$("#lihaAteriat").append( "<li class='collection-item ateria'>" + 
+				"<div class='col s12 ateriaKuva'>" + 
+				"<img class='ateriaKuva1' src='kuvat/safka2.png'>" + "</div>" +
+				"<div class='col s12 m7'>" +
+				"<span class='ateriaNimi'>" + result.nimi + "</span>" +
+				"<p class='ateriaTeksti'>" + result.ainekset + "</p>" + "</div>" +
+				"<div class='col s12 m5 erikoisRuoka'> <p class='erikoisRv'>" + result.ruokavalio + "</p> </div>" +
+				"<div class='col s12 m5 ateriaPohja'>" + 
+				"<p class='ateriaHinta'>" + result.hinta + " €" + "</p>" +
+				"<a class='ateriaNappi waves-effect waves-light btn-large green'>lisää ateria</a>" +
+				"</div>" + "</li>"
+				);
+			 // kalaruokakategorian ateriat
+			 }else if(result.ruokalaji == "kalaruoka"){
+				$("#kalaAteriat").append( "<li class='collection-item ateria'>" + 
+				"<div class='col s12 ateriaKuva'>" + 
+				"<img class='ateriaKuva1' src='kuvat/safka2.png'>" + "</div>" +
+				"<div class='col s12 m7'>" +
+				"<span class='ateriaNimi'>" + result.nimi + "</span>" +
+				"<p class='ateriaTeksti'>" + result.ainekset + "</p>" + "</div>" +
+				"<div class='col s12 m5 erikoisRuoka'> <p class='erikoisRv'>" + result.ruokavalio + "</p> </div>" +
+				"<div class='col s12 m5 ateriaPohja'>" + 
+				"<p class='ateriaHinta'>" + result.hinta + " €" + "</p>" +
+				"<a class='ateriaNappi waves-effect waves-light btn-large green'>lisää ateria</a>" +
+				"</div>" + "</li>"
+				);
+			 // kanaruokakategorian ateriat
+			 }else if(result.ruokalaji == "kanaruoka"){
+				$("#kanaAteriat").append( "<li class='collection-item ateria'>" + 
+				"<div class='col s12 ateriaKuva'>" + 
+				"<img class='ateriaKuva1' src='kuvat/safka2.png'>" + "</div>" +
+				"<div class='col s12 m7'>" +
+				"<span class='ateriaNimi'>" + result.nimi + "</span>" +
+				"<p class='ateriaTeksti'>" + result.ainekset + "</p>" + "</div>" +
+				"<div class='col s12 m5 erikoisRuoka'> <p class='erikoisRv'>" + result.ruokavalio + "</p> </div>" +
+				"<div class='col s12 m5 ateriaPohja'>" + 
+				"<p class='ateriaHinta'>" + result.hinta + " €" + "</p>" +
+				"<a class='ateriaNappi waves-effect waves-light btn-large green'>lisää ateria</a>" +
+				"</div>" + "</li>"
+				);
+			// kasvisruokakategorian ateriat	
+			}else if(result.ruokalaji == "kasvisruoka"){
+				$("#kasvisAteriat").append( "<li class='collection-item ateria'>" + 
+				"<div class='col s12 ateriaKuva'>" + 
+				"<img class='ateriaKuva1' src='kuvat/safka2.png'>" + "</div>" +
+				"<div class='col s12 m7'>" +
+				"<span class='ateriaNimi'>" + result.nimi + "</span>" +
+				"<p class='ateriaTeksti'>" + result.ainekset + "</p>" + "</div>" +
+				"<div class='col s12 m5 erikoisRuoka'> <p class='erikoisRv'>" + result.ruokavalio + "</p> </div>" +
+				"<div class='col s12 m5 ateriaPohja'>" + 
+				"<p class='ateriaHinta'>" + result.hinta + " €" + "</p>" +
+				"<a class='ateriaNappi waves-effect waves-light btn-large green'>lisää ateria</a>" +
+				"</div>" + "</li>"
+				);
+			}
+		});
+		
+	}
+	
+	// etusivunapin painallus
+    $("#etusivuNappi").click(function(){ // näytetään etusivu
+		$("#ateriaValikko").css("display", "none");
+		$("#loppuSivu").css("display", "none"); 
+		$("#etuSivu").css("display", "block");
+	});
 
 //************************************************************************************
 //TESTAUS
@@ -31,12 +116,7 @@ $(document).ready(function(){
 		$("#etuSivu").css("display", "block");
 		$("#loppuSivu").css("display", "block");
 	});
-	// etusivunapin painallus
-    $("#etusivuNappi").click(function(){ // näytetään etusivu
-		$("#ateriaValikko").css("display", "none");
-		$("#loppuSivu").css("display", "none"); 
-		$("#etuSivu").css("display", "block");
-	});
+	
 	// ateriavalikkonapin painallus
 	$("#ateriaNappi").click(function(){ // näytetään ateriavalikkosivu
 		$("#ateriaValikko").css("display", "block");
